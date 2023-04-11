fdfdce9a9b65b2f4eee3ab126c8ccf3e658bf191
pawaranurag123 <95554131+pawaranurag123@users.noreply.github.com>
2023-04-11 23:50:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,76 +8,16 @@ resource "aws_security_group" "my_security_group" {
   description = "security group for Ec2 instance"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
- ingress {
-    from_port   = 8042
-    to_port     = 8042
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   ingress {
-    from_port   = 1234
-    to_port     = 1234
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   ingress {
-    from_port   = 9870
-    to_port     = 9870
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   ingress {
-    from_port   = 8088
-    to_port     = 8088
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   ingress {
-    from_port   = 9000
-    to_port     = 9000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   ingress {
-    from_port   = 8888
-    to_port     = 8888
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-   ingress {
+
+ ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-    ingress {
-    from_port = -1
-    to_port = -1
-    protocol = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -95,52 +35,20 @@ resource "aws_security_group" "my_security_group" {
 }
 
 # Create AWS ec2 instance
-#resource "aws_instance" "myFirstInstance" {
-#  ami           = var.ami_id
-##  key_name = var.key_name
- # instance_type = var.instance_type
- # security_groups= [var.security_group]
- # tags= {
- #   Name = var.tag_name
- # }
-#}
-
-resource "aws_instance" "hdfs" {
-  ami           = "ami-00ce00765d9093e1e"
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
   security_groups= [var.security_group]
   tags= {
-    Name = "Jupyter-hdfs_demo"
+    Name = var.tag_name
   }
 }
 
-resource "aws_instance" "serving" {
-  ami           = "ami-07e936751c60cf84d"
-  key_name = var.key_name
-  instance_type = var.instance_type
-  security_groups= [var.security_group]
-  tags= {
-    Name = 	"mlflowServing_demo"
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "my_elastic_ip"
   }
-}
-
-resource "aws_instance" "datalake" {
-  ami           = "ami-07ce48306dfb01300"
-  key_name = var.key_name
-  instance_type = var.instance_type
-  security_groups= [var.security_group]
-  tags= {
-    Name = 	"backend-postgress_demo"
-  }
-}
-
-resource "aws_instance" "gitlab" {
-  ami           = "ami-090f4cb80df18fed9"
-  key_name = var.key_name
-  instance_type = var.instance_type
-  security_groups= [var.security_group]
-  tags= {
-    Name = 	"mlflow-gitlab_demo"
-  }
-}
