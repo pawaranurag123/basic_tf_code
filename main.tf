f0eab9a51618d6541a13e2e8104603ad0b5e785d
pawaranurag123 <95554131+pawaranurag123@users.noreply.github.com>
2023-04-12 09:54:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,12 @@ resource "aws_security_group" "my_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
  # outbound from jenkis server
   egress {
